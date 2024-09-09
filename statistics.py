32932762c8862c49b87e5a91f5c985661e41f3fe
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-09-09 14:36:01 +0700

    Fix flake8

--- statistics.py
+++ statistics.py
@@ -1,25 +1,30 @@
+"""Module for calculating statistical data."""
 from math import sqrt
+
+
 def average(data):
-    """Return the average of a list of numeric values in data."""
-    if len(data)==0:
+    """Calculate the average of a list of numeric values."""
+    if len(data) == 0:
         raise ValueError("List must contain at least one value")
-    return sum(data)/len(data)
+    return sum(data) / len(data)
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """
+    Calculate the population variance of a list of numbers.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
-    This is different from the Python library function statistics.variance
+    This is different from the Python library function statistics.variance,
     which returns the sample variance, where the sum is divided by (n-1).
 
-    Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
+    Example:
+        variance([1, 5]) is ((1 - 3) ** 2 + (5 - 3) ** 2) / 2 = 4.
 
-    :param data: list of numbers for which variance will be computed. 
-           Must contain at least one element.
-    :returns: population variance of values in data list.
-    :raises ValueError: if the data parameter is empty.
+    :param data: List of numbers for which variance will be computed.
+                 Must contain at least one element.
+    :returns: Population variance of values in data list.
+    :raises ValueError: If the data parameter is empty.
 
     >>> variance([1])
     0.0
@@ -30,15 +35,13 @@ def variance(data):
     >>> variance([1000000, 1000004])
     4.0
     """
-    # ugly code.
-    n=len(data)
-    if n==0:
+    n = len(data)
+    if n == 0:
         raise ValueError("List must contain at least one value")
     avg = average(data)
-    return sum( [(x-avg)**2 for x in data] )/n
+    return sum([(x - avg) ** 2 for x in data]) / n
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Calculate the standard deviation of a list of values."""
     return sqrt(variance(data))
-
